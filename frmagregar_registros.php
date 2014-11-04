be40d2d6933cb8eee178f9f8cf1fe951ff49afb9
oscantillomen <oscantillomen@misena.edu.co>
2014-11-04 10:59:24 -0500

    proyecto telefonia

---
+++ frmagregar_registros.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<head>
+  <LINK REL="SHORTCUT ICON" HREF="uninorte.ico" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Consultar Estado Equipo</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <style type="text/css">
+      #nuevo_registro{
+        background:red; 
+        width:20%;
+        height:50%;
+      }
+    </style>
+</head>
+<body>
+	<img style="width: 303px; height: 146px margin: -55px -216px -112px -140px;" src="images/jpg/logo.jpg" alt="Logo Universidad del Norte."/>
+  <body>
+  <nav class="top-bar" data-topbar>
+
+    <section class="top-bar-section">
+      <!-- Right Nav Section  menu lado derecho-->
+      <ul class="left">
+        <li><a href="#" text-decoration="none"><strong>AGREGAR REGISTRO/REGLETA<strong></a></li>
+      </ul>
+    </section>
+
+    <section class="top-bar-section">
+      <!-- Right Nav Section  menu lado derecho-->
+      <ul class="right">
+        <li><a href="index.html"><strong>INICIO<strong></a></li>
+      </ul>
+    </section>
+  </nav><br>
+	
+  <div class="centrado large-2" id="centrado">
+  	<input type="text" name="txtnom_registro" id="txtnom_registro" placeholder="Nombre Registro" class="columns centrado" required><br><br>
+  <input type="button" name="btncrear_registro" id="btncrear_registro" value="Crear Registro" class="button">
+</div>
+<div id="nuevo_registro " class="centrado large-2">
+  
+<input type="text" >
+</div>
+</body>
+</html>
